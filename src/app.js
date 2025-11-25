fc8eb249cdfa84bb66f2007587b4b1a2526a0053
Afsana Mim <afsanamim9639@gmail.com>
2025-11-26 04:39:45 +0600

    Added some configuration for deployment error

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: 'EcoTrack API is running!'
+  });
+});
+
+app.get('/api/health', (req, res) => {
+  res.json({
+    success: true,
+    status: 'OK'
+  });
+});
+
+module.exports = app;
