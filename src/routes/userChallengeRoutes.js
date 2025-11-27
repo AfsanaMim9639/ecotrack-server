0253491f1b4e99bd16a4fa83cb2a03bf5505dbc6
Afsana Mim <afsanamim9639@gmail.com>
2025-11-28 01:30:52 +0600

    Changed the vercel.jscon file

--- src/routes/userChallengeRoutes.js
+++ src/routes/userChallengeRoutes.js
@@ -3,7 +3,8 @@ import {
   joinChallenge,
   getUserChallenges,
   getUserChallengeById,
-  updateProgress
+  updateProgress,
+  abandonChallenge
 } from '../controllers/userChallengeController.js';
 import { authenticateUser } from '../middleware/auth.js';
 
@@ -12,9 +13,19 @@ const router = express.Router();
 // All routes require authentication
 router.use(authenticateUser);
 
+// Join challenge
 router.post('/join', joinChallenge);
+
+// Get all user challenges
 router.get('/', getUserChallenges);
+
+// Get one
 router.get('/:id', getUserChallengeById);
-router.post('/:id/progress', updateProgress);
+
+// Update progress (FIXED: must be PATCH)
+router.patch('/:id/progress', updateProgress);
+
+// Abandon challenge (missing route FIXED)
+router.patch('/:id/abandon', abandonChallenge);
 
 export default router;
